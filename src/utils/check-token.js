96b0f75176313d37510f8bfa951ee24408892d05
Alex <lisandra.nazarova@gmail.com>
2019-09-19 17:03:41 +0300

    .env

--- src/utils/check-token.js
+++ src/utils/check-token.js
@@ -1,10 +1,11 @@
 import jwt from 'jsonwebtoken';
 import axios from "axios";
 import {authTypes} from "../actions/types";
+require('dotenv').config();
 
 const checkTokenExpirationMiddleware = store => next => action => {
     const token = localStorage.getItem('token');
-    axios.defaults.baseURL = 'http://localhost:8000';
+    axios.defaults.baseURL = process.env.REACT_APP_API_PROTOCOL+'://'+process.env.REACT_APP_API_HOST;
     if (token) {
         axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
         if (jwt.decode(token).exp < Date.now() / 1000) {
